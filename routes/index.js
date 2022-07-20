5a19daac776cb0dccbae7e430a108acb22006d51
Emily Byrne <embyrne13@gmail.com>
2022-07-20 18:47:55 -0400

    plants

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+
+router.get('/', (req, res) => res.send('This is root!'))
+router.get('/plants', controllers.getAllPlants)
+router.post('/plants', controllers.createPlant)
+router.get('/plants/:id', controllers.getPlantById)
+router.put('/plants/:id', controllers.updatePlant)
+router.delete('/plants/:id', controllers.deletePlant)
+module.exports = router
